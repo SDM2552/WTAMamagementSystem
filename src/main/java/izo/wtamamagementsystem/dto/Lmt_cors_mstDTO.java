06ca3a7a2076360e5849aa9ca3ce6b82b8653fe3
idnodea <59271963+idnodea@users.noreply.github.com>
2024-05-07 17:08:05 +0900

    dto약간추가

--- src/main/java/izo/wtamamagementsystem/dto/Lmt_cors_mstDTO.java
+++ src/main/java/izo/wtamamagementsystem/dto/Lmt_cors_mstDTO.java
@@ -2,4 +2,17 @@ package izo.wtamamagementsystem.dto;
 
 //교육과정정보
 public class Lmt_cors_mstDTO {
+    private int idCors;
+    private String corsNm;
+    private String entYr;
+    private String selCd;
+    private String nthCd;
+    private String stasCorsDiv;
+    private String dtlCorsDiv;
+    private String corsCont;
+    private String timeBrf;
+    private String corsBrf;
+    private int stdCredit;
+    private String idSubject;
+
 }
---
+++ src/main/java/izo/wtamamagementsystem/entity/CourseDetails.java
@@ -0,0 +1,29 @@
+package izo.wtamamagementsystem.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course_details")
+public class CourseDetails {
+    @Id
+    @Column(name = "id_course_details")
+    private String idCourseDetails;
+
+    @Column(name = "id_student")
+    private String idStudent;
+
+    @Column(name = "id_lesson")
+    private String idLesson;
+
+    @Column(name = "grade")
+    private String grade;
+
+    // Getters and Setters
+}
