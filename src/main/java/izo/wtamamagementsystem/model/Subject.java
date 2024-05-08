ead5ba014535f818d7bd8d315c7bf2374c42e8ed
zxv5503 <zxv5503@naver.com>
2024-05-08 11:43:32 +0900

    .

---
+++ src/main/java/izo/wtamamagementsystem/model/Subject.java
@@ -0,0 +1,11 @@
+package izo.wtamamagementsystem.model;
+
+import lombok.Data;
+
+@Data
+public class Subject {
+    private String id_subject;  //과목
+    private String subject_name;    //과목이름
+    private String subject_type;    //과목구분
+    private String subject_code;    //과목코드
+}
