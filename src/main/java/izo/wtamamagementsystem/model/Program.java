3093bdf7b2c63a1a316e6a443422d5c7373e7c6e
zxv5503 <zxv5503@naver.com>
2024-05-09 16:22:06 +0900

    jiung

--- src/main/java/izo/wtamamagementsystem/model/Program.java
+++ src/main/java/izo/wtamamagementsystem/model/Program.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Program {
-    private int id_program; //교육과정코드
+    private int idProgram; //교육과정코드
     private String programName; //교육분류명
     private String progDesc;    //교육과정내용
 }
